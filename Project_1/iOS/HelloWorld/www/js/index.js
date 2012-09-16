c993fb6310c0cfc41112e983534724e9e785dc6c
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-09-16 10:58:53 -0500

    Updated Codrova.plist to allow external links/resources. Also updated index.html, css/index.css and js/indeex.js to to pull in API data from Twitter and Khan Academy. Working at this point.

--- Project_1/iOS/HelloWorld/www/js/index.js
+++ Project_1/iOS/HelloWorld/www/js/index.js
@@ -48,6 +48,7 @@ window.addEventListener("DOMContentLoaded", function() {
                 ge('dev-content').style.display = "block";
                 ge('native-content').style.display = "none";
                 ge('dataApi-content').style.display = "none";
+                ge('dataView').style.display = "none";
                 ge('research-content').style.display = "none";
                 ge('video-content').style.display = "none";
                 break;
@@ -56,6 +57,7 @@ window.addEventListener("DOMContentLoaded", function() {
                 ge('dev-content').style.display = "none";
                 ge('native-content').style.display = "block";
                 ge('dataApi-content').style.display = "none";
+                ge('dataView').style.display = "none";
                 ge('research-content').style.display = "none";
                 ge('video-content').style.display = "none";
                 break;
@@ -64,6 +66,7 @@ window.addEventListener("DOMContentLoaded", function() {
                 ge('dev-content').style.display = "none";
                 ge('native-content').style.display = "none";
                 ge('dataApi-content').style.display = "block";
+                ge('dataView').style.display = "block";
                 ge('research-content').style.display = "none";
                 ge('video-content').style.display = "none";
                 break;
@@ -72,6 +75,7 @@ window.addEventListener("DOMContentLoaded", function() {
                 ge('dev-content').style.display = "none";
                 ge('native-content').style.display = "none";
                 ge('dataApi-content').style.display = "none";
+                ge('dataView').style.display = "none";
                 ge('research-content').style.display = "block";
                 ge('video-content').style.display = "none";
                 break;
@@ -80,14 +84,82 @@ window.addEventListener("DOMContentLoaded", function() {
                 ge('dev-content').style.display = "none";
                 ge('native-content').style.display = "none";
                 ge('dataApi-content').style.display = "none";
+                ge('dataView').style.display = "none";
                 ge('research-content').style.display = "none";
                 ge('video-content').style.display = "block";
                 break;
             default:
                 return false;
-                        
         }
     };
+                        
+    // Twitter Feed Using jQuery //
+    // Load JSONp Data //
+    $('#twitterButton').on("click", function(){
+    console.log('#twitterButton');
+        $('#dataView').empty();
+        $.ajax({
+            url: 'http://search.twitter.com/search.json?q=%40fullsail',
+            type: 'GET',
+            dataType: 'jsonp',
+            success: function(data){
+            console.log(data);
+            alert("This only took " + data.completed_in + " seconds.");
+                $("<h2>").text("Twitter Results for @FullSail").appendTo("#dataView");
+                    for (var i=0, j=data.results.length; i<j; i++){
+                        var dr = data.results[i];
+                        $("<img />").attr({
+                            style: 'border-radius:6px;',
+                            src: dr.profile_image_url
+                        }).appendTo("#dataView");
+                        $(''+
+                            '<h2>'+ dr.from_user +'</h2>'+
+                            '<p>'+ dr.text +'</p>'+
+                            '<p>'+ dr.created_at +'</p>'
+                        ).appendTo('#dataView');
+                    };
+                },
+                error: function(error){
+                    console.log(error);
+                    alert("Please try again.\n" + error);
+                } 
+        });
+    });
+                        
+    // Khan Academy Using jQuery //
+    // Load JSONp Data //
+    $('#khanButton').on("click", function(){
+    console.log('#khanButton');
+        $('#dataView').empty();
+        $.ajax({
+            url: 'http://www.khanacademy.org/api/v1/playlists',
+            type: 'GET',
+            dataType: 'jsonp',
+            success: function(data){
+            console.log(data);
+            alert("Here comes the data from Khan Academy.");
+            $("<h2>").text("Khan Academy Playlists").appendTo("#dataView");
+                for (var i=0, j=data.length; i<j; i++){
+                    var dr = data[i];
+                    $("<img />").attr({
+                        style: 'border-radius:6px;',
+                        src: dr.profile_image_url
+                    }).appendTo("#dataView");
+                    $('<a>'+
+                        '<h2>'+ dr.community_questions_title +'</h2>'+
+                        '<p>'+ dr.description +'</p>'+
+                        '<p>'+ dr.backup_timestamp +'</p>'+
+                        '<small>'+ "Tags" +'</small>'+
+                        '<p>'+ dr.tags +'</p>'
+                    ).attr("href", "http://www.khanacademy.com" + dr.topic_page_url).appendTo('#dataView');
+                };
+            },
+            error: function(error){
+                console.log(error);
+                alert("Please try again.\n" + error);
+            }
+        });
+    });
                         
     // Add Event Listeners for Click Events - Then Run togglePage() According to Link Clicked //
     var displayDev = ge('dev');
